8717d0a7228faef40723aa915d32f4c97ad2487d
Dima Pristupa <dima@pristupa.dev>
2024-09-13 19:21:34 +0300

    Finish the "5.5. It blinks" chapter

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -2,14 +2,32 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit as _;
-use panic_halt as _;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
+use microbit::hal::timer::Timer;
+use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello World");
 
-    loop {}
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+
+        timer.delay_ms(1_000_u16);
+
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+
+        timer.delay_ms(1_000_u16);
+    }
 }
