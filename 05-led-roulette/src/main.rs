a34fa6d355cd2277cd91a4a11adccd4b04132df5
Dima Pristupa <dima@pristupa.dev>
2024-09-13 20:28:10 +0300

    Finish the "5.6. The challenge" chapter

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -2,32 +2,56 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
-use microbit::hal::prelude::*;
-use microbit::hal::timer::Timer;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    let mut board = Board::take().unwrap();
-
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let mut leds = [
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    let mut x = 0;
+    let mut y = 0;
+    let mut dx = 1i8;
+    let mut dy = 0i8;
 
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Dark!");
+        display.show(&mut timer, leds, 100);
 
-        timer.delay_ms(1_000_u16);
+        leds[y as usize][x as usize] = 0;
 
-        row1.set_high().unwrap();
-        rprintln!("Light!");
+        if x == 4 && dx == 1 {
+            dx = 0;
+            dy = 1;
+        } else if y == 4 && dy == 1 {
+            dx = -1;
+            dy = 0;
+        } else if x == 0 && dx == -1 {
+            dx = 0;
+            dy = -1;
+        } else if y == 0 && dy == -1 {
+            dx = 1;
+            dy = 0;
+        }
 
-        timer.delay_ms(1_000_u16);
+        x += dx;
+        y += dy;
+
+        leds[y as usize][x as usize] = 1;
     }
 }
